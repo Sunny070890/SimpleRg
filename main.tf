337692504c85f4ce2ed67f2055b222d5913ce9bf
Sunny070890 <sunnyknt4@gmail.com>
2025-07-23 15:52:13 +0530

    added required version

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
    required_version = ">= 1.3.0"
   required_providers {
-    azurerm = {
+    azurerm =        {
       source = "hashicorp/azurerm"
-      version = "4.37.0"
+      version =       "4.37.0"
     }
   }
 }
