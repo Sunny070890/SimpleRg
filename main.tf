dfca4c43a3e56c7a52bbcebb693ff7a6a1a92c0b
Sunny070890 <sunnyknt4@gmail.com>
2025-07-23 14:30:19 +0530

    Added RG Code

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+  subscription_id = "08a0553e-c405-4797-8f3c-4ea48da302a9"
+  
+}
+resource "azurerm_resource_group" "rg1" {
+  name = "sk-rg"
+  location = "eastus"
+}
